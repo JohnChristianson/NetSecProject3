3bb0e78308db9af70f714407accf06f34da6879c
JohnAChris <JohnAChristianson@gmail.com>
2023-10-23 20:24:34 -0500

    bing chat

--- Project3A.c
+++ Project3A.c
@@ -1,98 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+// A structure to hold the certificate fields
+typedef struct {
+    char version[4];
+    char serialNumber[20];
+    char signatureAlgorithm[50];
+    char issuer[256];
+    char validityNotBefore[20];
+    char validityNotAfter[20];
+    char subject[256];
+    char subjectPublicKeyInfo[512];
+} Certificate;
+
+// A function to write the certificate to a file
+void writeCertificate(const char *filename, const Certificate *cert) {
+    FILE *file = fopen(filename, "w");
+    if (!file) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    fprintf(file, "Version: %s\n", cert->version);
+    fprintf(file, "Serial Number: %s\n", cert->serialNumber);
+    fprintf(file, "Signature Algorithm: %s\n", cert->signatureAlgorithm);
+    fprintf(file, "Issuer: %s\n", cert->issuer);
+    fprintf(file, "Validity Not Before: %s\n", cert->validityNotBefore);
+    fprintf(file, "Validity Not After: %s\n", cert->validityNotAfter);
+    fprintf(file, "Subject: %s\n", cert->subject);
+    fprintf(file, "Subject Public Key Info: %s\n", cert->subjectPublicKeyInfo);
+
+    fclose(file);
+}
+
+// A function to read the certificate from a file
+void readCertificate(const char *filename, Certificate *cert) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    fscanf(file, "Version: %s\n", cert->version);
+    fscanf(file, "Serial Number: %s\n", cert->serialNumber);
+    fscanf(file, "Signature Algorithm: %s\n", cert->signatureAlgorithm);
+    fscanf(file, "Issuer: %[^\\n]\n", cert->issuer);
+    fscanf(file, "Validity Not Before: %s\n", cert->validityNotBefore);
+    fscanf(file, "Validity Not After: %s\n", cert->validityNotAfter);
+    fscanf(file, "Subject: %[^\\n]\n", cert->subject);
+    fscanf(file, "Subject Public Key Info: %[^\\n]\n", cert->subjectPublicKeyInfo);
+
+    fclose(file);
+}
 
 int main() {
-
-    struct signatureAlgIdentifier {
-        int algorithm;          // Algorithm
-        int params;             // Parameters
+    Certificate cert = {
+        .version = "1",
+        .serialNumber = "1234567890",
+        .signatureAlgorithm = "sha256WithRSAEncryption",
+        .issuer = "CN=Test Certificate Authority,O=Test Org,C=US",
+        .validityNotBefore = "20230101000000Z",
+        .validityNotAfter = "20240101000000Z",
+        .subject = "CN=Test Subject,O=Test Org,C=US",
+        .subjectPublicKeyInfo = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtvr9T8ZU2HKSig..."
     };
 
-    struct validity {
-        int startDate;          // Period of validity start
-        int endDate;            // Period of validity end
-    };
+    // Write the certificate to a file
+    writeCertificate("certificate.txt", &cert);
 
-    struct subjectInfo {
-        int algorithms;         // Algorithms
-        int params;             // Parameters
-        int key;                // Key
-    };
+    // Read the certificate from the file
+    Certificate readCert;
+    readCertificate("certificate.txt", &readCert);
 
-    struct signature {
-        int algs;
-        int params;
-        int signature;
-    };
+    // Print the read certificate to verify it was read correctly
+    printf("Read certificate:\n");
+    printf("Version: %s\n", readCert.version);
+    
+   // ... repeat for other fields ...
 
-    struct certificate {
-        int version;            // Version
-        int serialNumber;       // Certificate serial number.
-
-        // Signature algorithm identifier
-        struct signatureAlgIdentifier sigAlgIdent;
-
-        char issuerName[100];   // Issuer name
-
-        // Period of validity
-        struct validity validPeriod;
-
-        char subjectName[100];  // Subject name
-
-        // Subject public key info
-        struct subjectInfo subInfo;
-
-        // Signature
-        struct signature sig;
-
-        short TL;                 // Trust level
-    };
-
-    struct certificate cert;
-
-    printf("Enter Version Number: ");
-    scanf("%d", &cert.version);
-
-    printf("\nEnter Certificate Serial Number: ");
-    scanf("%d", &cert.serialNumber);
-
-    printf("\nEnter Signature Algorithm Identifier - Algorithm: ");
-    scanf("%d", &cert.sigAlgIdent.algorithm);
-
-    printf("\nEnter Signature Algorithm Identifier - Parameters: ");
-    scanf("%d", &cert.sigAlgIdent.params);
-
-    printf("\nEnter Issuer Name: ");
-    scanf("%s", cert.issuerName);
-
-    printf("\nEnter Period of Validity Start: ");
-    scanf("%d", &cert.validPeriod.startDate);
-
-    printf("\nEnter Period of Validity End: ");
-    scanf("%d", &cert.validPeriod.endDate);
-
-    printf("\nEnter Subject Name");
-    scanf("%s", &cert.subjectName);
-
-    printf("\nEnter Subject Public Key Info - Algorithms: ");
-    scanf("%d", &cert.subInfo.algorithms);
-
-    printf("\nEnter Subject Public Key Info - Parameters: ");
-    scanf("%d", &cert.subInfo.params);
-
-    printf("\nEnter Subject Public Key Info - Key: ");
-    scanf("%d", &cert.subInfo.key);
-
-    printf("\n Enter Signature Algorithms: ");
-    scanf("%d", cert.sig.algs);
-
-    printf("\n Enter Signature Parameters: ");
-    scanf("%d", cert.sig.params);   
-
-    printf("\n Enter Signature: ");
-    scanf("%d", cert.sig.signature);
-
-
-    return 0;
+   return 0;
 }
