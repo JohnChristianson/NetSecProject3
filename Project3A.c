6855f848db2ad538a36faaade0572187f28b201b
JohnAChris <JohnAChristianson@gmail.com>
2023-12-03 16:17:07 -0600

    readCertificate() isues

--- Project3A.c
+++ Project3A.c
@@ -15,7 +15,7 @@ typedef struct {
     char validityNotAfter[20];
     char subject[256];
     char subjectPublicKeyInfo[512];
-    int trustLevel;
+    char trustLevel[4];
 } Certificate;
 
 // A function to write the certificate to a file
@@ -34,7 +34,7 @@ void writeCertificate(const char *filename, const Certificate *cert) {
     fprintf(file, "Validity Not After: %s\n", cert->validityNotAfter);
     fprintf(file, "Subject: %s\n", cert->subject);
     fprintf(file, "Subject Public Key Info: %s\n", cert->subjectPublicKeyInfo);
-    fprintf(file, "Trust Level: %d\n", cert->trustLevel);
+    fprintf(file, "Trust Level: %s\n", cert->trustLevel);
 
     fclose(file);
 }
@@ -48,40 +48,44 @@ void readCertificate(const char *filename, Certificate *cert) {
     
     fscanf(file, "Version: %s\n", cert->version);
     fscanf(file, "Serial Number: %s\n", cert->serialNumber);
+
     fscanf(file, "Signature Algorithm: %s\n", cert->signatureAlgorithm);
-    fscanf(file, "Issuer: %[^\\n]\n", cert->issuer);
+    fscanf(file, "Issuer: %s\n", cert->issuer);
+    printf(cert->issuer);
+
     fscanf(file, "Validity Not Before: %s\n", cert->validityNotBefore);
     fscanf(file, "Validity Not After: %s\n", cert->validityNotAfter);
-    fscanf(file, "Subject: %[^\\n]\n", cert->subject);
-    fscanf(file, "Subject Public Key Info: %[^\\n]\n", cert->subjectPublicKeyInfo);
-    fscanf(file, "Trust Level: %d", cert->trustLevel);
+    fscanf(file, "Subject: %s\n", cert->subject);
+    fscanf(file, "Subject Public Key Info: %s\n", cert->subjectPublicKeyInfo);
+    fscanf(file, "Trust Level: %s\n", cert->trustLevel);
 
     fclose(file);
 }
 
-bool verifyCert(Certificate cert, char* currentDate, char* currentHash) {
+bool verifyCert(Certificate* cert, char* currentDate, unsigned char currentHash) {
     //returns true if cert is valid, false otherwise
+    printf("%s\n", cert->validityNotBefore);
 
     //Check if current date is before "valid not before" date
-    if (strcmp(cert.validityNotBefore, currentDate) > 0) {
+    if (strcmp(cert->validityNotBefore, currentDate) > 0) {
         printf("Cert is not valid. Current date is before certificate validity start.");
         return false;
     }
 
     //Check if current date is after "valid not after" date
-    if (strcmp(cert.validityNotAfter, currentDate) > 0) {
+    if (strcmp(cert->validityNotAfter, currentDate) > 0) {
         printf("Cert is not valid. Current date is after certificate validity end.");
         return false;
     }
 
 
     //Check if certificate hashes to the same value
-    char storedHash[1000];
     FILE* hashFile = fopen("hash.txt", "r");
-    fscanf(hashFile, "%s", storedHash);
+    char storedHash = fgetc(hashFile);
+    
     fclose(hashFile);
 
-    if (strcmp(storedHash, currentHash) != 0) {
+    if (storedHash != currentHash) {
         printf("Cert is not valid. Cert has been modified - hashes do not match.");
         return false;
     }
@@ -91,13 +95,13 @@ bool verifyCert(Certificate cert, char* currentDate, char* currentHash) {
 
 int main() {
     //Manually change the "current date" to check if certificate is within its valid timeframe
-    char currentDate[] = "20231128204400";// YYYY MM DD HH MM SS
+    char currentDate[] = "20201128204400";// YYYY MM DD HH MM SS
 
     // Read the certificate from the file
     Certificate readCert;
     readCertificate("certificate.txt", &readCert);
 
-    Certificate cert = {
+    /*Certificate cert = {
         .version = readCert.version,
         .serialNumber = readCert.serialNumber,
         .signatureAlgorithm = readCert.signatureAlgorithm,
@@ -107,33 +111,21 @@ int main() {
         .subject = readCert.subject,
         .subjectPublicKeyInfo = readCert.subjectPublicKeyInfo,
         .trustLevel = readCert.trustLevel
-    };
+    };*/
 
-    // Write the certificate to a file
-    //writeCertificate("certificate.txt", &cert);
 
-    // Print the read certificate to verify it was read correctly
-    system("clear");
-    printf("Read certificate:\n", 0);
-    printf("Version: %s\n", readCert.version);
-    
+
     // Hash the certificate
-
-    FILE* certFile = fopen("certificate.txt", "r");
-    if (!certFile) {
-        printf("Unable to open certFile");
-        return EXIT_FAILURE;
-    }
-
-    printf("AAAAA");
     bool flag = false;
     char c, ch;
     long long hashKey = 1234;
     keys(hashKey);
 
+    FILE* hashFileCert = fopen("certificate.txt", "r");
+    
     do {
-        c = fgetc(certFile);
-        if (feof(certFile)) {
+        c = fgetc(hashFileCert);
+        if (feof(hashFileCert)) {
             break;
         }
 
@@ -145,16 +137,19 @@ int main() {
         }
     } while(1);
 
-    fclose(certFile);
+    fclose(hashFileCert);
 
     unsigned char uch = ch;
-    printf("Hash: %x", uch);
+    //printf("Hash: %x\n", uch);
 
+    /*
     FILE* hashFile = fopen("hash.txt", "w");
     fprintf(hashFile, "%c\n", uch);
     fclose(hashFile);
+    */
 
-    bool certIsValid = verifyCert(cert, currentDate, uch);
+    bool certIsValid = verifyCert(&readCert, currentDate, uch);
+    printf("Cert is valid: %d", certIsValid);
 
    return 0;
 }
